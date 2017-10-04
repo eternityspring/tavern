9982df79eb8f567beff3763621322d3736cf6fd5
eternityspring@163.com <1526qaws>
2017-10-05 00:11:41 +0800

    custom menu

--- pages/eat/eat.js
+++ pages/eat/eat.js
@@ -1,20 +1,20 @@
-
 Page({
   data: {
     result: '什么',
     book : [
       {
-        breakfast: "面包 蛋糕 荷包蛋 烧饼 饽饽 油条 馄饨 火腿 面条 小笼包  玉米粥 肉包 山东煎饼 饺子 煎蛋 烧卖 生煎 锅贴 包子 酸奶 苹果 梨 香蕉 皮蛋瘦肉粥 蛋挞 南瓜粥 煎饼 玉米糊 泡面 粥 馒头 燕麦片 水煮蛋 米粉 豆浆 牛奶 花卷 豆腐脑 煎饼果子 小米粥 黑米糕 鸡蛋饼 牛奶布丁 水果沙拉 鸡蛋羹 南瓜馅饼 鸡蛋灌饼 奶香小馒头 汉堡包 披萨 八宝粥 三明治 蛋包饭 豆沙红薯饼 驴肉火烧 粥 粢饭糕 蒸饺 白粥"
+        content: "面包 蛋糕 荷包蛋 烧饼 饽饽 油条 馄饨 火腿 面条 小笼包 玉米粥 肉包 山东煎饼 饺子 煎蛋 烧卖 生煎 锅贴 包子 酸奶 苹果 梨 香蕉 皮蛋瘦肉粥 蛋挞 南瓜粥 煎饼 玉米糊 泡面 粥 馒头 燕麦片 水煮蛋 米粉 豆浆 牛奶 花卷 豆腐脑 煎饼果子 小米粥 黑米糕 鸡蛋饼 牛奶布丁 水果沙拉 鸡蛋羹 南瓜馅饼 鸡蛋灌饼 奶香小馒头 汉堡包 披萨 八宝粥 三明治 蛋包饭 豆沙红薯饼 驴肉火烧 粥 粢饭糕 蒸饺 白粥"
       },
       {
-        lunch: "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉"
+        content: "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉"
       },
       {
-        dinner: "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉"
+        content: "盖浇饭 砂锅 大排档 米线 满汉全席 西餐 麻辣烫 自助餐 炒面 快餐 水果 西北风 馄饨 火锅 烧烤 泡面 速冻水饺 日本料理 涮羊肉 味千拉面 肯德基 面包 扬州炒饭 自助餐 茶餐厅 海底捞 咖啡 比萨 麦当劳 兰州拉面 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 粥 快餐 萨莉亚 桂林米粉 东南亚菜 甜点 农家菜 川菜 粤菜 湘菜 本帮菜 竹笋烤肉"
       }
     ],
     selectCount:0,
     randomShow:false,
+    menuShow:false,
     randomData:[],
     top:[],
     left:[],
@@ -25,8 +25,18 @@ Page({
   },
   onLoad: function () {
     var self = this;
+    var hour = (new Date).getHours();
+    var bookIdx = 0;
+    // 根据当前时间 选择不同食谱
+    if(hour < 9 || hour >= 23){
+      bookIdx = 0;
+    }else if(hour<13){
+      bookIdx = 1;
+    }else{
+      bookIdx = 2;
+    }
     self.setData({
-      randomData:this.data.book[0].breakfast.split(" ")
+      randomData:this.data.book[bookIdx].content
     });
     self.getWindowsInfo(function (res) {
       self.setData({
@@ -47,7 +57,7 @@ Page({
     var leftTemp = [];
     var opacityTemp = [];
     var self = this;
-    for(let i=0;i<self.data.randomData.length;i++){
+    for(let i=0;i<self.data.randomData.split(" ").length;i++){
       topTemp.push(Math.ceil(Math.random()*(height-50)))
       leftTemp.push(Math.ceil(Math.random()*width))
       opacityTemp.push(Math.random())
@@ -66,9 +76,9 @@ Page({
       selectCount:self.data.selectCount+1
     })
     // 设置吃的结果
-    let resultIndex = Math.floor(Math.random()*self.data.randomData.length);
+    let resultIndex = Math.floor(Math.random()*self.data.randomData.split(" ").length);
     self.setData({
-      result:self.data.randomData[resultIndex]
+      result:self.data.randomData.split(" ")[resultIndex]
     })
   },
   startRandom(){
@@ -96,9 +106,27 @@ Page({
         randomFlag:null
       })
     }
-    // 显示数据风暴
+    // 隐藏数据风暴
     self.setData({
       randomShow:false,
     })
-  }
+  },
+  showMenu(){
+    var self = this;
+    self.setData({
+      menuShow:true,
+    })
+  },
+  hideMenu(){
+    var self = this;
+    self.setData({
+      menuShow:false,
+    })
+  },
+  setMenuData(e){
+    var self = this;
+    self.setData({
+      randomData:e.detail.value.replace(/\s+/g,' '),
+    })
+  },
 })
