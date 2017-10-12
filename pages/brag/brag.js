9323c570cdd93df54479f88a04666cac35c14688
eternityspring@163.com <1526qaws>
2017-10-12 20:31:25 +0800

    brag

--- pages/brag/brag.js
+++ pages/brag/brag.js
@@ -1,11 +1,28 @@
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    randomNum:0,
+    randomArray:[],
   },
   onLoad: function () {
     this.setData({
-      
+      randomNum:Math.floor((Math.random()*6)+1)
+    })
+    this.setRandomData();
+  },
+  random:function () {
+    this.setData({
+      randomNum:Math.floor((Math.random()*6)+1)
+    })
+  },
+  setRandomData(){
+    var tempArr = [];
+    for(let i = 0;i<5;i++){
+      tempArr.push(Math.floor((Math.random()*6)+1))
+    }
+    this.setData({
+      randomArray:tempArr
     })
   }
 })
